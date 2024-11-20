2a780f748156c3302e1b77932f362c37e5fffd4a
Алексей Б <loja.bobaev2000@yandex.ru>
2024-11-20 23:56:30 +0300

    add: vue-routing и пути приложения

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+import Wethers from './views/wethers/Wethers.vue'
+import Main from './views/main/Main.vue'
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: "/", component: Main },
+        { path: "/weathers", component: Wethers },
+    ]
+})
+
+createApp(App).use(router).mount('#app');
